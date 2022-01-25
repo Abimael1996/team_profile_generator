116b8cce3e0c7f74d3a100b9dfcd96b00f5ea25f
abi <abimael.monarrez58@gmail.com>
2022-01-25 02:40:39 -0700

    add file with methods that return pieces of an html file

---
+++ src/template.js
@@ -0,0 +1,46 @@
+class Template {
+    constructor(){
+
+    }
+
+    managerName(name) {
+      return `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+            crossorigin="anonymous"
+            />
+            <link
+              rel="stylesheet"
+              href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            />
+            <link rel="stylesheet" href="style.css" />
+            <title>My Team</title>
+          </head>
+        
+          <body>
+            <header>
+              <h1>My Team</h1>
+            </header>
+            
+            <main>
+              <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${name}</h5>
+                  <h4 class="card-title"><i class="fas fa-mug-hot"></i> Manager</h4>
+                </div>`
+    }
+
+    managerID(id) {
+      return ` <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${id}</li>`
+    }
+}
+
+module.exports = Template;
