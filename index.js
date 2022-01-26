332b5b47192866445d6fa85a19e876bb7f36c58b
abi <abimael.monarrez58@gmail.com>
2022-01-26 00:26:23 -0700

    add two more prompts through methods

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const fs = require("fs");
 const template = new Template();
 const employee = new Employee;
 const htmlPath = "./dist/index.html"
-const questions = ["What is your team's manager's name?", "What is your eam's manager's ID number?"]
+const questions = ["What is your team's manager's name?", "What is your team's manager's ID number?", "What is your team's manager's email address?"]
 
 inquirer
-  .prompt([employee.getName(questions[0])])
+  .prompt([employee.getName(questions[0]), employee.getId(questions[1]), employee.getEmail(questions[2])])
   .then((name) => { 
-      const managerName = template.managerName(name.manager);
+      const managerInfo = template.manager(name.name, name.id, name.email);
       
-      fs.appendFile(htmlPath, managerName, (err) => {
+      fs.appendFile(htmlPath, managerInfo, (err) => {
           if(err) {
               console.log(err);
               }
