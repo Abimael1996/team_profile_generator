a04b02efc54ee8874c9fe78c5c13aa9ac8e232fa
abi <abimael.monarrez58@gmail.com>
2022-01-26 15:56:54 -0700

    reorganize template dividing it by each part of the html file, plus the css file script

--- src/template.js
+++ src/template.js
@@ -1,44 +1,91 @@
 class Template {
-    constructor(){
+  constructor(){
+    
+  }
 
-    }
+  css() {
+    return `header {
+      text-align: center;
+      background-color: red;
+      color: white;
+      padding: 40px 0 40px 0;
+  }
+  
+  main {
+      display: flex;
+      flex-wrap: wrap;
+      margin: 20px 10% 0 10%;
+      width: 80%;
+      justify-content: center;
+  }
+  
+  .card-body {
+      background-color: blue;
+      color: white;
+  }
+  
+  ul {
+      color: black;
+  }
+  
+  .card {
+      margin-right: 20px;
+      margin-top: 20px;
+      border-width: 4px;
+  }`
+  }
 
-    manager(name, id, email) {
-      return `<!DOCTYPE html>
-        <html lang="en">
-          <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-            <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-            crossorigin="anonymous"
-            />
-            <link
-              rel="stylesheet"
-              href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            />
-            <link rel="stylesheet" href="style.css" />
-            <title>My Team</title>
-          </head>
+  openHtml() {
+    return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+        crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        />
+        <link rel="stylesheet" href="style.css" />
+        <title>My Team</title>
+      </head>
+    
+      <body>
+        <header>
+          <h1>My Team</h1>
+        </header>
         
-          <body>
-            <header>
-              <h1>My Team</h1>
-            </header>
-            
-            <main>
-              <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">${name}</h5>
-                  <h4 class="card-title"><i class="fas fa-mug-hot"></i> Manager</h4>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">ID: ${id}</li>
-                  <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>`
-    }
+        <main>`
+  }
+
+  employee(name, id, email, unique, role) {
+    return `
+            <div class="card" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <h4 class="card-title">${role}</h4>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">${unique}</li>
+                </ul>
+              </div>`
+  }
+
+  closeHtml() {
+    return `
+      </main>
+    </body>
+  </html>`
+
+  }
 
 }
 
