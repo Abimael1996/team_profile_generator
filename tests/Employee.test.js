411aa4ccb4625ee655539357cb6fa568cdd8a05f
abi <abimael.monarrez58@gmail.com>
2022-01-26 00:33:36 -0700

    employee.getId test passed

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,14 +2,24 @@ const Employee = require('../lib/Employee');
 
 
 describe('Manager Info', () => {
-  it('should return an object that asks for the name of the manager', () => {
+  it("should return an object that asks for the manager's name", () => {
 
     const employee = new Employee();
     const question = "What's the name of the manager?";
 
     employee.getName(question);
 
-    expect.objectContaining({ message: "What's the name of the manager?"});
+    expect.objectContaining({ message: question});
+  }
+  )
+  it("should return an object that asks for the manager's ID", () => {
+
+    const employee = new Employee();
+    const question = "What's the manager's ID?";
+
+    employee.getId(question);
+
+    expect.objectContaining({ message: question});
   }
   )
 });
